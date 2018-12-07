cb79a592917f65202322345bb6f1932c1a0dbd44
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2018-12-06 23:59:13 -0600

    Uploading the navigation layout

--- src/App.js
+++ src/App.js
@@ -4,9 +4,29 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
-       <h1>Hello world</h1>
-      </div>
+      <div className="demo-big-content">
+    <Layout>
+        <Header title="Title" scroll>
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+        </Content>
+    </Layout>
+</div>
     );
   }
 }
