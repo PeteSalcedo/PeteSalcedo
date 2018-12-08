ec062422432031328d40152a96806fc2117d1cba
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2018-12-08 14:42:28 -0600

    Uploading styled nava bar with gradient

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import {Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from "./components/main"
 import { Link } from 'react-router-dom';
+import "./App.css"
 
 class App extends Component {
   render() {
     return (
       <div className="demo-big-content">
     <Layout>
-        <Header title="Title" scroll>
+        <Header className="header-color" title="Title" scroll>
             <Navigation>
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
@@ -18,10 +19,10 @@ class App extends Component {
         </Header>
         <Drawer title="Title">
             <Navigation>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Drawer>
         <Content>
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-
+import { Grid, Cell } from 'react-mdl';
 
 class Landing extends Component {
-    render() {
-        return(
-            <div><h1>Landing Page</h1></div>
-        )
-    }
+	render() {
+		return (
+			<div style={{ width: '100%', margin: 'auto' }}>
+				<Grid className="landing-grid">
+					<Cell col={12}>1</Cell>
+
+				</Grid>
+			</div>
+		);
+	}
 }
 
 export default Landing;
