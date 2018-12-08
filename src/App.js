417da9caf2f84ca7e2d74fec5484711f8d10089b
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2018-12-08 16:50:24 -0600

    Uploading the main landing page complete

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
 import React, { Component } from 'react';
-import {Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-import Main from "./components/main"
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
 import { Link } from 'react-router-dom';
-import "./App.css"
+import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="demo-big-content">
-    <Layout>
-        <Header className="header-color" title="Title" scroll>
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Header>
-        <Drawer title="Title">
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Main/>
-        </Content>
-    </Layout>
-</div>
-    );
-  }
+	render() {
+		return (
+			<div className="demo-big-content">
+				<Layout>
+					<Header className="header-color" title="Find Out More" scroll>
+						<Navigation>
+							<Link to="/resume">Resume</Link>
+							<Link to="/aboutme">About Me</Link>
+							<Link to="/projects">Projects</Link>
+							<Link to="/contact">Contact</Link>
+						</Navigation>
+					</Header>
+					<Drawer title="Pete Salcedo">
+						<Navigation>
+							<Link to="/resume">Resume</Link>
+							<Link to="/aboutme">About Me</Link>
+							<Link to="/projects">Projects</Link>
+							<Link to="/contact">Contact</Link>
+						</Navigation>
+					</Drawer>
+					<Content>
+						<div className="page-content" />
+						<Main />
+					</Content>
+				</Layout>
+			</div>
+		);
+	}
 }
 
 export default App;
