4172b005fc2c5f59d0655d361a49efd5c58d68fa
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2018-12-08 20:58:26 -0600

    React material ui tabs finished

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl'
 
 
 class Project extends Component {
+    constructor(props){
+        super(props);
+        this.state = {active: 0};
+    }
     render() {
         return(
-            <div><h1>Project Page</h1></div>
+            <div className="category-tabs">
+              <Tabs activeTab={this.state.activeTab} onChange={(tabId)=> this.setState({ activeTab: tabId })} ripple>
+              <Tab>React JS</Tab>
+              <Tab>Hola Code</Tab>
+              <Tab>Socket IO</Tab>
+              <Tab>Angular</Tab>
+              </Tabs>              
+            </div>
         )
     }
 }
